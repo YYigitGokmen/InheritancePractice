83166a0762a1d6acc7246a74e123d51184c3e759
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-02 16:11:07 +0300

    Add files via upload

---
+++ BaseKisi.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePractice
+{
+    public abstract class BaseKisi
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+
+
+        public BaseKisi(string name,string surname)
+        {
+        Name = name;
+        Surname = surname;
+
+        }
+
+        public void Promter()
+        {
+            Console.WriteLine($"Adınız {Name}, Soyadınız {Surname}");
+        }
+
+    }
+
+
+    public class Student : BaseKisi
+    {
+        public string? StudentId { get; set; }
+
+        public Student(string name, string surname, string studentId)
+            : base(name, surname)
+        {
+            StudentId = studentId;
+        }
+
+
+        public void StudentMethod()
+        {
+            Console.WriteLine($"Öğrenci numaranız: {StudentId}");
+
+            Promter();
+            //Dikkat ->  Bir metot içerisinde başka bir metodu çağırabileceğinizi unutmayınız.
+
+        }
+    }
+
+
+        public class Teacher : BaseKisi
+        {
+            //Ogretmen için ilave propertyler -> Maaş Bilgisi
+            public int TeacherSalary { get; set; }
+
+        
+            public Teacher(string name, string surname, int teacherSalary)
+                : base(name, surname)
+            {
+                TeacherSalary = teacherSalary;
+            }
+        
+            public void TeacherMethod() 
+        {
+            Console.WriteLine($"Öğretmen Maaşınız: {TeacherSalary}");
+
+            Promter();
+
+        
+
+        }
+
+
+
+    }
+
+
+    }
+
+
+
+    
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using InheritancePractice;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+
+        Student student = new Student("Yiğit","GÖKMEN","123456789");
+
+        student.StudentMethod();
+
+        Console.WriteLine("---------------------------");
+
+
+        Teacher teacher = new Teacher("HASAN","ALİ",100);
+
+        teacher.TeacherMethod();
+
+
+    }
+}
+
+
+
+
